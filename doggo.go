1f2b0de3ed0aacdab98ee44acac880e52da669a5
Tuomas Louekari <tuoppil@gmail.com>
2021-08-29 16:55:00 +0300

    Refreshing memory on Go

---
+++ doggo.go
@@ -0,0 +1,19 @@
+package main
+
+import "io/ioutil"
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := ioutil.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+}
