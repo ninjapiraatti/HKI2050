de4c047c13497f4735831e2f0b37be94621c050a
ninjapiraatti <tuoppil@gmail.com>
2021-09-18 10:20:34 +0300

    Changed paths again

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ async fn main() -> std::io::Result<()> {
 			.service(home)
 			.service(allviews)
 			.service(web::resource("/").route(web::get().to(|req: HttpRequest| {
-				HttpResponse::Found().header(header::LOCATION, "index.html").finish()
+				HttpResponse::Found().header(header::LOCATION, "/public/index.html").finish()
 			})))
 	})
 	.bind(server_url)?
